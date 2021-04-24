b46dd3be38d859ce707f02f06087f31e8378d3df
Alexander Kaistra <Alexander.Kaistra@gmx.de>
2021-04-24 14:07:33 +0200

    Ein kleines Hello-World Programm um damit warm zu werden.

---
+++ Abgabenordner/Uebungsblatt02/Einzelabgaben/Kaistra/Augustiner.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+
+
+}
