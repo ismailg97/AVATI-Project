b318281e032906a21171e2dd4b4ab355bd103701
hubanton <anton.huber98@yahoo.com>
2021-05-02 13:43:49 +0200

    löschen blatt2einzel huber

--- Abgabenordner/Uebungsblatt02/Einzelabgaben/Huber/Team12/Team12/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ Abgabenordner/Uebungsblatt02/Einzelabgaben/Huber/Team12/Team12/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace Team12.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Tobi\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
+#line 1 "C:\Users\anton\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Tobi\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
+#line 2 "C:\Users\anton\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Tobi\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
+#line 3 "C:\Users\anton\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Tobi\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
+#line 4 "C:\Users\anton\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Tobi\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
+#line 5 "C:\Users\anton\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Tobi\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
+#line 6 "C:\Users\anton\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Tobi\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
+#line 7 "C:\Users\anton\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Tobi\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
+#line 8 "C:\Users\anton\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Tobi\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
+#line 9 "C:\Users\anton\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
 using Team12;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Tobi\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
+#line 10 "C:\Users\anton\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\_Imports.razor"
 using Team12.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using Team12.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 23 "C:\Users\Tobi\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\Shared\NavMenu.razor"
+#line 23 "C:\Users\anton\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Huber\Team12\Team12\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
