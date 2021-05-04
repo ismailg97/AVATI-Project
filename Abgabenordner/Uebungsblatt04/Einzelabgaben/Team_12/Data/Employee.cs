e8315a517c6d139cc071528f40b3a7841641d096
Tobias Mayr <tobi-mayr@arcor.de>
2021-05-04 19:12:11 +0200

    Aufagbe 4a) und b) hinzugefügt

---
+++ Abgabenordner/Uebungsblatt04/Einzelabgaben/Team_12/Data/Employee.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Team_12.Data
+{
+    public class Employee
+    {
+        public String Firstname { get; set; }
+        public String Lastname { get; set; }
+        public String Fin_Projects { get; set; }
+        public DateTime Day_of_Birth { get; set; }
+    }
+}
