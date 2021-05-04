41020d83c422f2ab8c2f76162c70ed5b5336a586
hubanton <anton.huber98@yahoo.com>
2021-05-04 16:39:10 +0200

    update

---
+++ Abgabenordner/Uebungsblatt04/Musterloesung/Data/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team12.Data
+{
+    public class Employee
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public DateTime Geburtstag { get; set; }
+
+        public string PrevProjects { get; set; }
+        
+
+    }
+}
