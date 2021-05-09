8420bee51544e10bc578e4bda4cdf9ad5fe33513
Alexander Kaistra <Alexander.Kaistra@gmx.de>
2021-05-09 23:05:00 +0200

    Änderungen nochmal durchchecken

--- Abgabenordner/Uebungsblatt02/Einzelabgaben/Mayr/Team_12/Team_12/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ Abgabenordner/Uebungsblatt02/Einzelabgaben/Mayr/Team_12/Team_12/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,70 +13,110 @@ namespace Team_12.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
+<<<<<<< Updated upstream
 #line 1 "C:\Users\ismai\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+=======
+#line 1 "C:\Users\Alexander\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+>>>>>>> Stashed changes
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
+<<<<<<< Updated upstream
 #line 2 "C:\Users\ismai\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+=======
+#line 2 "C:\Users\Alexander\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+>>>>>>> Stashed changes
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
+<<<<<<< Updated upstream
 #line 3 "C:\Users\ismai\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+=======
+#line 3 "C:\Users\Alexander\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+>>>>>>> Stashed changes
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
+<<<<<<< Updated upstream
 #line 4 "C:\Users\ismai\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+=======
+#line 4 "C:\Users\Alexander\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+>>>>>>> Stashed changes
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
+<<<<<<< Updated upstream
 #line 5 "C:\Users\ismai\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+=======
+#line 5 "C:\Users\Alexander\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+>>>>>>> Stashed changes
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
+<<<<<<< Updated upstream
 #line 6 "C:\Users\ismai\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+=======
+#line 6 "C:\Users\Alexander\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+>>>>>>> Stashed changes
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
+<<<<<<< Updated upstream
 #line 7 "C:\Users\ismai\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+=======
+#line 7 "C:\Users\Alexander\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+>>>>>>> Stashed changes
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
+<<<<<<< Updated upstream
 #line 8 "C:\Users\ismai\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+=======
+#line 8 "C:\Users\Alexander\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+>>>>>>> Stashed changes
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
+<<<<<<< Updated upstream
 #line 9 "C:\Users\ismai\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+=======
+#line 9 "C:\Users\Alexander\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+>>>>>>> Stashed changes
 using Team_12;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
+<<<<<<< Updated upstream
 #line 10 "C:\Users\ismai\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+=======
+#line 10 "C:\Users\Alexander\Source\Repos\tutorium-f-team-12\Abgabenordner\Uebungsblatt02\Einzelabgaben\Mayr\Team_12\Team_12\_Imports.razor"
+>>>>>>> Stashed changes
 using Team_12.Shared;
 
 #line default
