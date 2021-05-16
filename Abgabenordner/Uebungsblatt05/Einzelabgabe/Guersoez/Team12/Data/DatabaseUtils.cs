1f7706645f7ff63b153457e1da904a0fa72bd00f
DESKTOP-15GNDRC\ismai <ismail.guersoez@student.uni-augsburg.de>
2021-05-16 18:44:38 +0200

    Einzelabgabe Gürsöz

---
+++ Abgabenordner/Uebungsblatt05/Einzelabgabe/Guersoez/Team12/Data/DatabaseUtils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Collections;
+using System.Data.Common;
+using Microsoft.AspNetCore.Components;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Team12.Data;
+
+
+namespace Team12.Data
+{
+    public class DatabaseUtils
+    {
+        IConfiguration _configuration;
+
+        public DatabaseUtils(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbConnection GetDbConnection()
+        {
+            return new SqlConnection("SoPro2021");
+        }
+
+        public bool ErzeugeTabelle()
+        {
+            DbConnection db = GetDbConnection();
+            db.Open();
+
+            return true;
+
+        }
+
+    }
+}
---
+++ Abgabenordner/Uebungsblatt05/Einzelabgabe/Guersoez/Team12/Data/SkillServiceSimple.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+
+namespace Team12.Data
+{
+
+
+
+
+
+    public class SkillServiceSimple 
+    {
+        //private List<Skill> SkillList;
+
+        public List<Skill> AllSkills = new List<Skill>();
+        public bool DeleteSkill(int skillId)
+        {
+            int ausgabe = AllSkills.RemoveAll(x => x.Id == skillId);
+            if (ausgabe == 1) return true;
+            else return false;
+        }
+
+        public List<Skill> GetAllSkills()
+        {
+            return AllSkills;
+        }
+
+        public Skill GetSkill(int skillId)
+        {
+            return AllSkills.Find(x => x.Id == skillId);
+        }
+
+        public bool UpdateSkill(Skill skill)
+        {
+            Skill oldskill = AllSkills.Find(x => x.Id == skill.Id);
+            if (oldskill.Equals(null)) return false;
+            oldskill = skill;
+            return true;
+        }
+
+
+    }
+}
