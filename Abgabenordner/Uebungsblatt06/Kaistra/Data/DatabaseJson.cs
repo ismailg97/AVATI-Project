0cb0925e1db9a7cc8910f579531f9c41cb3bff6e
Alexander Kaistra <Alexander.Kaistra@gmx.de>
2021-05-19 15:55:44 +0200

    databasejson hochladen

---
+++ Abgabenordner/Uebungsblatt06/Kaistra/Data/DatabaseJson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Collection.Generic;
+using Newtonsoft.Json; 
+using Newtonsoft.Json.Serialization;
+
+
+
+public class DatabaseJson
+{
+    using (StreamReader r = new StreamReader("C:\Users\Alexander\Desktop\asdf\datenbasis.json"))
+    {
+        
+    }
+}
