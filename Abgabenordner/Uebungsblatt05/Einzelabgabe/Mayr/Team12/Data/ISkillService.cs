0f33c13e835612b9e2af49abd7260e9e0b6c484f
Tobias Mayr <tobi-mayr@arcor.de>
2021-05-19 23:04:20 +0200

    Blatt05 nachgereicht (jetzt wirklich)

---
+++ Abgabenordner/Uebungsblatt05/Einzelabgabe/Mayr/Team12/Data/ISkillService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+namespace Team12.Data {
+    public interface ISkillService {
+        Skill GetSkill(int skillId);
+        List<Skill> GetAllSkills();
+        bool UpdateSkill(Skill skill);
+        bool DeleteSkill(int skillId);
+    }
+}
