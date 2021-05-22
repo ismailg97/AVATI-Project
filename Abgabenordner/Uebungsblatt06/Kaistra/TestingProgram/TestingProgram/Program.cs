f182afb274ad5bff0d63371a492ba4a5daafaba7
Alexander Kaistra <Alexander.Kaistra@gmx.de>
2021-05-22 20:36:50 +0200

    Hochladen Unit tests

---
+++ Abgabenordner/Uebungsblatt06/Kaistra/TestingProgram/TestingProgram/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestingProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GeneriereDokument();
+        }
+        
+        [Test]
+        public static void test2Creation()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("https://localhost:5001");
+            
+            IWebElement linkTuEtwas = webDriver.FindElement(By.LinkText("tu etwas"));
+            linkTuEtwas.Click();
+            Thread.Sleep(500);
+            IWebElement linkAdd = webDriver.FindElement(By.LinkText("Add"));
+            linkAdd.Click();
+            Thread.Sleep(500);
+            IWebElement eingabeSkill= webDriver.FindElement(By.Id("name"));
+            eingabeSkill.SendKeys("testProgramm");
+            IWebElement submitButton = webDriver.FindElement(By.LinkText("Submit"));
+            submitButton.Click();
+            submitButton.Click();   //will nicht aktivieren
+            Thread.Sleep(500);
+            Assert.AreEqual(webDriver.Url, "https://localhost:5001/skill/edit");
+            IWebElement succes = webDriver.FindElement(By.Name("testProgramm"));
+            Console.WriteLine(succes.Text);
+            
+        }
+
+        public static void GeneriereDokument()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("https://localhost:5001");
+
+            Thread.Sleep(200);
+            IWebElement linkGeneriere = webDriver.FindElement(By.XPath("//button/Generiere Word DOC Kaistra"));
+            linkGeneriere.Click();
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
