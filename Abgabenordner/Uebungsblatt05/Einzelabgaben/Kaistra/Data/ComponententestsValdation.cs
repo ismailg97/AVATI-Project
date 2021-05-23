f5d42362bc06d7ecd123f6c25aab5971e442e2ce
Alexander Kaistra <Alexander.Kaistra@gmx.de>
2021-05-23 16:43:08 +0200

    Hochladen Hausaufgabe

---
+++ Abgabenordner/Uebungsblatt05/Einzelabgaben/Kaistra/Data/ComponententestsValdation.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Team12.Data
+{
+    public class ComponententestsValidation.cs
+    {
+    
+    [SetUp]
+        public void SetUp()
+        {
+        
+        }
+    }
+}
--- Abgabenordner/Uebungsblatt06/Kaistra/ComponententestsValdation.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Team12
-{
-    public class ComponententestsValdation{
-    
-    }
-
-}
