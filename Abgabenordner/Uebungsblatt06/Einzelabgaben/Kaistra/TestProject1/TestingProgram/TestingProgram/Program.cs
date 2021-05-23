ea6b854089bd99e5d85a6b65c586e6d37b137dfe
Alexander Kaistra <Alexander.Kaistra@gmx.de>
2021-05-23 18:22:57 +0200

    funze endlich

--- Abgabenordner/Uebungsblatt06/Einzelabgaben/Kaistra/TestProject1/TestingProgram/TestingProgram/Program.cs
+++ Abgabenordner/Uebungsblatt06/Einzelabgaben/Kaistra/TestProject1/TestingProgram/TestingProgram/Program.cs
@@ -19,21 +19,20 @@ namespace TestingProgram
             IWebDriver webDriver = new ChromeDriver();
             webDriver.Navigate().GoToUrl("https://localhost:5001");
 
-            var linkTuEtwas = webDriver.FindElement(By.LinkText("tu etwas"));
+            var linkTuEtwas = webDriver.FindElement(By.Id("blep"));
             linkTuEtwas.Click();
-            Thread.Sleep(500);
-            var linkAdd = webDriver.FindElement(By.LinkText("Add"));
+            Thread.Sleep(1500);
+            var linkAdd = webDriver.FindElement(By.Id("ADDIERE"));
             linkAdd.Click();
-            Thread.Sleep(500);
+            Thread.Sleep(1500);
             var eingabeSkill = webDriver.FindElement(By.Id("name"));
             eingabeSkill.SendKeys("testProgramm");
-            var submitButton = webDriver.FindElement(By.LinkText("Submit"));
+            var submitButton = webDriver.FindElement(By.Id("Submit"));
             submitButton.Click();
             submitButton.Click(); //will nicht aktivieren
-            Thread.Sleep(500);
-            Assert.AreEqual(webDriver.Url, "https://localhost:5001/skill/edit");
-            var succes = webDriver.FindElement(By.Name("testProgramm"));
-            Console.WriteLine(succes.Text);
+            Thread.Sleep(1500);
+            Assert.AreEqual(webDriver.Url, "https://localhost:5001/skill/index");
+            
         }
 
         public static void GeneriereDokument()
@@ -41,8 +40,8 @@ namespace TestingProgram
             IWebDriver webDriver = new ChromeDriver();
             webDriver.Navigate().GoToUrl("https://localhost:5001");
 
-            Thread.Sleep(200);
-            var linkGeneriere = webDriver.FindElement(By.XPath("//button/Generiere Word DOC Kaistra"));
+            Thread.Sleep(2000);
+            var linkGeneriere = webDriver.FindElement(By.Id("download Kaistra"));
             linkGeneriere.Click();
         }
     }
