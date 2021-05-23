5e71958521703d4ba2237c23557767ce9a29cefa
DESKTOP-15GNDRC\ismai <ismail.guersoez@student.uni-augsburg.de>
2021-05-23 22:16:16 +0200

    Einzelabgabe Blatt06-Guersoez

---
+++ Abgabenordner/Uebungsblatt05/Musterloesung/Team12/Data/Team12/Data/Team12/Data/ISkillService.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Team12.Data
+{
+    public interface ISkillService
+    {
+        
+        public Skill GetSkill(int id);
+
+
+        public List<Skill> GetAllSkills();
+
+
+        public bool UpdateSkill(Skill skill);
+
+
+        public bool DeleteSkill(int id);
+
+    }
+}
---
+++ Abgabenordner/Uebungsblatt05/Musterloesung/Team12/Data/Team12/Data/Team12/Data/Team12/Data/Team12/Data/Team12/Data/Team12/Data/Team12/Data/Team12/Data/Team12/Data/Team12/Data/IDatenbasis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team12.Data
+{
+    public interface IDatenbasis
+    {
+        public List<Skill> LoadJson();
+    }
+}
