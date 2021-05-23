efa5e8fbeba890b27bdbd2651bb4b7c1d1eca583
Alexander Kaistra <Alexander.Kaistra@gmx.de>
2021-05-23 16:56:26 +0200

    Hochladen der hausaufgabe.. again

---
+++ Abgabenordner/Uebungsblatt06/Einzelabgaben/Kaistra/Team12/Data/Employee.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Employee
+{
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public DateTime Day_of_Birth { get; set; }
+    public string[] Projects { get; set; }
+}
