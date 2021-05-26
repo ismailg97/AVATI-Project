0a2091e054b7033124cf238182b23343bf804aa6
Tobias Mayr <tobi-mayr@arcor.de>
2021-05-26 23:25:42 +0200

    blatt06 Mayr update

--- Abgabenordner/Uebungsblatt06/Einzelabgaben/Mayr/Team12/Data/SkillNameConventionAttribut.cs
+++ Abgabenordner/Uebungsblatt06/Einzelabgaben/Mayr/Team12/Data/SkillNameConventionAttribut.cs
@@ -1,13 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace Team12.Data
 {
     public class SkillNameConventionAttribut : ValidationAttribute
     {
+        private Regex regex = new Regex(@"^[a-zA-Z0-9\s]+$");
+        private string GetErrorMessage() => $"Invalid Name for Skill of type 'Softskill'";
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            //validationContext.
+            var skill = (Skill) validationContext.ObjectInstance;
+            if (skill.SkillType.Equals(Skill.Category.Softskill))
+            {
+                if (regex.IsMatch(skill.Name))
+                    return ValidationResult.Success;
+                return new ValidationResult(GetErrorMessage());
+            }
             return ValidationResult.Success;
         }
     }
 }
+
+//° ^ ! " § $ % & / ( ) = ? \ ` ´ + * ~ # ' - _ . : , ; > | < { [ ] } 
