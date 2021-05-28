06cd233d91321987329c4a84fd01a5fa163ff28a
Tobias Mayr <tobi-mayr@arcor.de>
2021-05-28 02:02:48 +0200

    Einzelabgabe complete ALLE

---
+++ Abgabenordner/Uebungsblatt06/Einzelabgaben/Mayr/Team12/TestProject1/End2EndTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using Team12.Data;
+
+namespace TestProject1
+{
+    public class End2EndTest
+    {
+        private IWebDriver _firefox;
+
+        [SetUp]
+        public void Setup() {
+            _firefox = new FirefoxDriver();
+        }
+
+        [Test]
+        public void TestEditSkill() {
+            _firefox.Navigate().GoToUrl("http://localhost:5000/skill/edit");
+            System.Threading.Thread.Sleep(750);
+
+            IWebElement input = _firefox.FindElement(By.Id("name"));
+            input.SendKeys("New SoftSkill");
+            SelectElement select = new SelectElement(_firefox.FindElement(By.Id("category")));
+            select.SelectByText("Soft-Skill");
+            _firefox.FindElement(By.Id("sub")).Click();
+            Assert.IsNotNull(SkillNameConventionAttribut.ErrorMessage);
+        }
+    }
+}
