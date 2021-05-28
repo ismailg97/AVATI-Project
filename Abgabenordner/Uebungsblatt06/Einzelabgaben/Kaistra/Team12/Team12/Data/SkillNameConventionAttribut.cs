4ccbe6c3fad5a368eb706fc8c1e891d43cc8364f
Alexander Kaistra <Alexander.Kaistra@gmx.de>
2021-05-28 04:51:34 +0200

    hopefully final

---
+++ Abgabenordner/Uebungsblatt06/Einzelabgaben/Kaistra/Team12/Team12/Data/SkillNameConventionAttribut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Team12.Data // source.: https://www.youtube.com/watch?v=o_AH2MGti0A
+{ //source1.: https://docs.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-5.0  | Custom attributes
+    public class SkillNameConventionAttribut : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var caster = (Skill) validationContext.ObjectInstance;
+            var data = " äöü$§%!&/()=?"; //chars that arent allowed
+            var falseChar = new List<char>();
+            falseChar.AddRange(data);
+
+            if (caster.type == Skilltype.Softskill)
+                if (caster.Name.Contains('ä') || caster.Name.Contains('ö') || caster.Name.Contains('!') ||
+                    caster.Name.Contains('ö') || caster.Name.Contains(')') || caster.Name.Contains('§') ||
+                    caster.Name.Contains('&') || caster.Name.Contains('(') || caster.Name.Contains('-') ||
+                    caster.Name.Contains('%') || caster.Name.Contains('/') || caster.Name.Contains('=') ||
+                    caster.Name.Contains('`') || caster.Name.Contains('´') ||
+                    caster.Name.Contains('^')) //gibts safe ne andere einfachere möglichkeit
+                
+                    return new ValidationResult("Error.: Fehler bei der Eingabe");
+            return ValidationResult.Success;
+        }
+    }
+}
