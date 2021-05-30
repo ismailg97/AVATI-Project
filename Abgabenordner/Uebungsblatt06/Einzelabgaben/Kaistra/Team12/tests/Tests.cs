bc9ffad36096d70a47ad9418e0ef08370ce2b1a7
Alexander Kaistra <Alexander.Kaistra@gmx.de>
2021-05-30 14:36:33 +0200

    firefox tests added

--- Abgabenordner/Uebungsblatt06/Einzelabgaben/Kaistra/Team12/tests/Tests.cs
+++ Abgabenordner/Uebungsblatt06/Einzelabgaben/Kaistra/Team12/tests/Tests.cs
@@ -2,15 +2,17 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace TestProject1
 {
     public class Program
     {
         [Test]
-        public static void Test2Creation()
+        public static void Test2CreationChrome()
         {
             IWebDriver webDriver = new ChromeDriver();
+
             webDriver.Navigate().GoToUrl("https://localhost:5001");
 
             Thread.Sleep(2000);
@@ -26,11 +28,45 @@ namespace TestProject1
             submitButton.Click();
         }
         
+        [Test]
+        public static void Test2CreationFirefox()
+        {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.AcceptInsecureCertificates = true;
+
+            IWebDriver webDriver = new FirefoxDriver(firefoxOptions);
+            webDriver.Navigate().GoToUrl("https://localhost:5001");
+
+            Thread.Sleep(2000);
+            var linkTuEtwas = webDriver.FindElement(By.Id("blep"));
+            linkTuEtwas.Click();
+            Thread.Sleep(2000);
+            var linkAdd = webDriver.FindElement(By.Id("ADDIERE"));
+            linkAdd.Click();
+            Thread.Sleep(2000);
+            var eingabeSkill = webDriver.FindElement(By.Id("name"));
+            eingabeSkill.SendKeys("testProgramm");
+            var submitButton = webDriver.FindElement(By.Id("Submit"));
+            submitButton.Click();
+        }
         
         [Test]
-        public static void GeneriereDokument()
+        public static void GeneriereDokumentChrome()
         {
             IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("https://localhost:5001");
+
+            Thread.Sleep(2000);
+            var linkGeneriere = webDriver.FindElement(By.Id("download Kaistra"));
+            linkGeneriere.Click();
+        }
+        [Test]
+        public static void GeneriereDokumentFirefox()
+        {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.AcceptInsecureCertificates = true;
+
+            IWebDriver webDriver = new FirefoxDriver(firefoxOptions);
             webDriver.Navigate().GoToUrl("https://localhost:5001");
 
             Thread.Sleep(2000);
