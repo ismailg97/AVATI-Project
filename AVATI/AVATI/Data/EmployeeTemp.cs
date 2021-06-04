f56cf94782995f25dc2ece6a92e626b38d4d903f
hubanton <anton.huber98@yahoo.com>
2021-06-04 14:02:59 +0200

    Added logik to EmployeeTemp.cs / RC-Level can now be changed in ProposalOverview.razor / Added Dummy-Proposals

--- AVATI/AVATI/Data/EmployeeTemp.cs
+++ AVATI/AVATI/Data/EmployeeTemp.cs
@@ -1,7 +1,14 @@
-﻿namespace AVATI.Data
+﻿using System.Collections.Generic;
+
+namespace AVATI.Data
 {
     public class EmployeeTemp
     {
-        
+        public string Name { get; set; }
+        public int RC { get; set; } = 1;
+        public List<Hardskill> Hardskills { get; set; }
+        public List<string> Sotfskills { get; set; }
+        public List<string> fields { get; set; }
+        public List<string> roles { get; set; }
     }
 }
--- AVATI/AVATI/Startup.cs
+++ AVATI/AVATI/Startup.cs
@@ -37,7 +37,8 @@ namespace AVATI
             services.AddSingleton<ProjectServiceSimple>();
             services.AddSingleton<IProjektService, ProjectServiceSimple>();
             services.AddSingleton<AddFieldsToProjectUpdate>();
-            
+            services.AddSingleton<ProposalService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
