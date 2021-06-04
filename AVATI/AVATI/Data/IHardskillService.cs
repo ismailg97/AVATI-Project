c4e07948f216528206ad51855182da3f558f91f9
DESKTOP-JTSAVUS\HP <victoria.kuch@student.uni-augsburg.de>
2021-06-04 14:10:56 +0200

    Update function in HardskillService and BasicDataService, add DeleteConfirmation and EditSoftskill

--- AVATI/AVATI/Data/IHardskillService.cs
+++ AVATI/AVATI/Data/IHardskillService.cs
@@ -5,14 +5,14 @@ namespace AVATI.Data
     public interface IHardskillService
     {
         public bool CreateHardskill( Hardskill hardskill );
-        public bool UpdateHardskill( Hardskill hardskill );
+        public bool UpdateHardskill( Hardskill newHardskill, Hardskill oldHardskill );
         public bool DeleteHardskill( string description );
 
         public Hardskill GetHardskill( string description );
         public List<Hardskill> GetAllHardskills();
         
         public bool CreateHardskillCategory( Hardskill hardskillcat );
-        public bool UpdateHardskillCategory( Hardskill hardskillcat );
+        public bool UpdateHardskillCategory( Hardskill newHardskillcat, Hardskill oldHardskillcat );
         public bool DeleteHardskillCategory( string description );
         
         public Hardskill GetHardskillCategory( string description );
