b842cd35f00a1d770e9ffdc51cf7f94b1141e277
Alexander Kaistra <Alexander.Kaistra@gmx.de>
2021-06-10 14:52:21 +0200

    @using directive added

--- AVATI/AVATI/Startup.cs
+++ AVATI/AVATI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using AVATI.Data;
 using AVATI.Pages;
+using AVATI.Pages.Project;
 using BlazorDownloadFile;
 
 namespace AVATI
