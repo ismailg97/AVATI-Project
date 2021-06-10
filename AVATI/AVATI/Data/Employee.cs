2c3f855952fc50a6f390505cb4e748743efff400
Tobias Mayr <tobi-mayr@arcor.de>
2021-06-10 20:59:08 +0200

    delete onInit

--- AVATI/AVATI/Data/Employee.cs
+++ AVATI/AVATI/Data/Employee.cs
@@ -12,16 +12,16 @@ namespace AVATI.Data
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Image;
-        public List<string> RelevantWorkExperience { get; set; }
+        public List<string> RelevantWorkExperience { get; set; } = new List<string>();
         public DateTime EmploymentTime { get; set; }
         public int Rc { get; set; }
-        public List<string> Softskills { get; set; }
-        public List<Hardskill> Hardskills { get; set; }
-        public List<string> Field { get; set; }
-        public List<string> Roles { get; set; }
-        public List<Tuple<string,LanguageLevel>> Language { get; set; }
-        public EmployeeType EmpType { get; set; }
+        public List<string> Softskills { get; set; } = new List<string>();
+        public List<Hardskill> Hardskills { get; set; } = new List<Hardskill>();
+        public List<string> Field { get; set; } = new List<string>();
+        public List<string> Roles { get; set; } = new List<string>();
+        public List<Tuple<string, LanguageLevel>> Language { get; set; } = new List<Tuple<string, LanguageLevel>>();
+        public EmployeeType EmpType { get; set; } 
         public bool IsActive  { get; set; } = true;
-        public List<Project> Projects { get; set; }
+        public List<Project> Projects { get; set; } = new List<Project>();
     }
 }
