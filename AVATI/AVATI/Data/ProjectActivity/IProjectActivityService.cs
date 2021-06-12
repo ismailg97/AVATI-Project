2d21ba6dc0a85f1554a909751d042eb3092672fc
DESKTOP-15GNDRC\ismai <ismail.guersoez@student.uni-augsburg.de>
2021-06-12 13:05:01 +0200

    Added LanguageLevel

--- AVATI/AVATI/Data/ProjectActivity/IProjectActivityService.cs
+++ AVATI/AVATI/Data/ProjectActivity/IProjectActivityService.cs
@@ -6,8 +6,12 @@ namespace AVATI.Data
     {
         public bool SetProjectActivity(int EmployeeId, int ProjectId, string Description);
         public bool DeleteProjectActivity(int EmployeeId, int ProjectId);
+     
         public ProjectActivity GetProjectActivities(int EmployeeId, int ProjectId);
         //Fehler im Designklassendiagramm
-        
+        public List<ProjectActivity> GetProjectActivitiesList();
+        public List<ProjectActivity> GetProjectActivityListEmployee(int EmployeeId);
+        public List<ProjectActivity> GetProjectActivityListProject(int ProjectId);
+
     }
 }
