9b261452052ddc159c86545b43972fc6214ca5db
DESKTOP-15GNDRC\ismai <ismail.guersoez@student.uni-augsburg.de>
2021-07-06 17:50:20 +0200

    Updated Mitarbeiterprofil Added Profile Picture Added Logic to Project Activity

--- AVATI/AVATI/Data/Employee.cs
+++ AVATI/AVATI/Data/Employee.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
+using System.Net.Mime;
 using Blazorise;
 using Microsoft.AspNetCore.Components;
 
@@ -13,11 +14,15 @@ namespace AVATI.Data
 {
     public class Employee
     {
-        public int EmployeeID { get; set; }
-        public string FirstName { get; set; }
         [NotNull][Required]
+        public int EmployeeID { get; set; }
+        
+        [NotNull][Required(ErrorMessage = "Vorname ist erforderlich")]
+        public string FirstName { get; set; }
+        
+        [NotNull][Required(ErrorMessage = "Nachname ist erforderlich")]
         public string LastName { get; set; }
-        public string Image;
+        public string Image { get; set; }
         public float RelevantWorkExperience { get; set; }
         public DateTime EmploymentTime { get; set; }
         public int Rc { get; set; }
@@ -28,7 +33,11 @@ namespace AVATI.Data
         public List<string> Roles { get; set; } = new List<string>();
         public List<string> LanguageName { get; set; } = new List<string>();
         public List<Tuple<string, LanguageLevel>> Language { get; set; } = new List<Tuple<string, LanguageLevel>>();
+        
+        [NotNull]
         public EmployeeType EmpType { get; set; }
+        
+        [NotNull]
         public bool IsActive  { get; set; } = true;
         public List<ProjectActivity> ProjectActivities { get; set; } = new List<ProjectActivity>();
     }
