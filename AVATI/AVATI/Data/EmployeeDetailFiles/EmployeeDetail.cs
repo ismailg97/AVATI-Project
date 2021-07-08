f6cc3b1da7d776162c5dfa320c66cb1cb6e60fd2
hubanton <anton.huber98@yahoo.com>
2021-07-08 18:37:16 +0200

    added discount option and aligned buttons properly

--- AVATI/AVATI/Data/EmployeeDetailFiles/EmployeeDetail.cs
+++ AVATI/AVATI/Data/EmployeeDetailFiles/EmployeeDetail.cs
@@ -17,6 +17,7 @@ namespace AVATI.Data.EmployeeDetailFiles
         public List<string> Roles { get; set; } = new List<string>();
         public List<Tuple<string, LanguageLevel>> Languages { get; set; } = new List<Tuple<string, LanguageLevel>>();
 
-        
+        [Range(0, 100, ErrorMessage = "Rabat muss zwischen 0 und 100% liegen")]
+        public int Discount;
     }
 }
