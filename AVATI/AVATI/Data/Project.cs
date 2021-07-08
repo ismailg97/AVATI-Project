8730c9d382a08528927d352fd03c85b0e734cc62
Alexander Kaistra <Alexander.Kaistra@gmx.de>
2021-07-08 17:21:44 +0200

    hochladen änderungen Projektzwecke und kleinere dinge am Projektedit

--- AVATI/AVATI/Data/Project.cs
+++ AVATI/AVATI/Data/Project.cs
@@ -9,21 +9,19 @@ namespace AVATI.Data
     [DateTimeValidationAttribute]
     public class Project
     {
-        public int ProjectID { get; set; }          
+        public int ProjectID { get; set; }
         
         [Required(ErrorMessage = "Name ist erforderlich")]
         [StringLength(70, ErrorMessage = "Projekttitel ist zu lang (70 Zeichen)")]
-        public string Projecttitel { get; set; }    
+        public string Projecttitel { get; set; }
         
         [StringLength(150, ErrorMessage = "Projektbeschreibung ist zu lang (150 Zeichen)")]
-        public string Projectdescription { get; set; }  
-        public List<ProjectPurpose> Projectpurpose { get; set; } = new List<ProjectPurpose>(); 
-        public List<ProjectActivity> ProjectActivities { get; set; } = new List<ProjectActivity>(); 
-        
-        public DateTime Projectbeginning { get; set; }  
-        
-        public DateTime Projectend { get; set; }    
-        public List<string> Fields { get; set; } = new List<string>();  
-        public List<Employee> Employees { get; set; } = new List<Employee>(); 
+        public string Projectdescription { get; set; }
+        public List<ProjectPurpose> Projectpurpose { get; set; } = new List<ProjectPurpose>();
+        public List<ProjectActivity> ProjectActivities { get; set; } = new List<ProjectActivity>();
+        public DateTime Projectbeginning { get; set; }
+        public DateTime Projectend { get; set; }
+        public List<string> Fields { get; set; } = new List<string>();
+        public List<Employee> Employees { get; set; } = new List<Employee>();
     }
 }
