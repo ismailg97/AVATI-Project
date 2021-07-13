8b96cd3ef0004f0cffe9de3284f14862e6244c98
DESKTOP-15GNDRC\ismai <ismail.guersoez@student.uni-augsburg.de>
2021-07-13 23:19:14 +0200

    Added Cookies

--- AVATI/AVATI/Data/CookieController.cs
+++ AVATI/AVATI/Data/CookieController.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace AVATI.Data
 {
@@ -11,13 +13,31 @@ namespace AVATI.Data
     [ApiController]
     public class CookieController : ControllerBase
     {
-        [HttpPost]
-        public async Task<ActionResult> Login([FromForm] string name, [FromForm] string empType)
+        private int Id;
+        private readonly IEmployeeService _employeeService;
+        private readonly ILoginService _loginService;
+
+        public CookieController(ILoginService loginService, IEmployeeService employeeService)
         {
+            _loginService = loginService;
+            _employeeService = employeeService;
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult> Login([FromForm] string username, [FromForm] string password)
+        {
+            
+            Id = _loginService.LogIn(username, password);
+            if (Id == -1)
+            {
+                return BadRequest();
+            }
+            Employee emp = _employeeService.GetEmployeeProfile(Id);
+            Console.WriteLine(emp.FirstName + emp.LastName);
             ClaimsIdentity claimsIdentity = new ClaimsIdentity(new List<Claim>
             {
-                new(ClaimTypes.Name, name),
-                new(ClaimTypes.Role, empType)
+                new(ClaimTypes.NameIdentifier, emp.EmployeeID.ToString()),
+                new(ClaimTypes.Role, emp.EmpType.ToString())
             }, CookieAuthenticationDefaults.AuthenticationScheme);
             ClaimsPrincipal claims = new ClaimsPrincipal(claimsIdentity);
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claims);
