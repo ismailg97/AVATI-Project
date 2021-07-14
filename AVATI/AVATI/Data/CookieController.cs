aa6ec1a9559a599bc6c0be4a8347bc51a96d0f64
DESKTOP-15GNDRC\ismai <ismail.guersoez@student.uni-augsburg.de>
2021-07-15 01:44:00 +0200

    Added Redirect to Login if not authorized

--- AVATI/AVATI/Data/CookieController.cs
+++ AVATI/AVATI/Data/CookieController.cs
@@ -4,6 +4,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -30,7 +31,7 @@ namespace AVATI.Data
             Id = _loginService.LogIn(username, password);
             if (Id == -1)
             {
-                return BadRequest();
+                return Redirect("/LoginFail");
             }
             Employee emp = _employeeService.GetEmployeeProfile(Id);
             Console.WriteLine(emp.FirstName + emp.LastName);
@@ -42,6 +43,28 @@ namespace AVATI.Data
             ClaimsPrincipal claims = new ClaimsPrincipal(claimsIdentity);
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claims);
             return Redirect("/");
+        }
+        
+        
+        [HttpDelete]
+        public void LogOut()
+        {
+            Console.WriteLine("TEST");
+            if (HttpContext != null)
+            {
+                //_httpContextAccessor.HttpContext.Response.Cookies.Delete(".AspNetCore.Cookies", new CookieOptions()
+                //{
+                //    Secure = true,
+                //});
+                CookieOptions option = new CookieOptions();
+                option.Expires = DateTime.Now.AddDays(-1);
+                option.Secure = true;
+                option.IsEssential = true;
+                HttpContext.Response.Cookies.Append(".AspNetCore.Cookies", string.Empty, option);
+                //Then delete the cookie
+                HttpContext.Response.Cookies.Delete(".AspNetCore.Cookies");
+            }
+            //return Redirect("login"); 
         }
     }
 }
