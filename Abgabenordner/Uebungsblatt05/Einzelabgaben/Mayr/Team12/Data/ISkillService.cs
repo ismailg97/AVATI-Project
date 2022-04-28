e8853ef29526985d5ff4766a30b3f1b812f09d92
ismailg97 <104563365+ismailg97@users.noreply.github.com>
2022-04-28 10:23:49 +0000

    Update

--- Abgabenordner/Uebungsblatt05/Einzelabgaben/Mayr/Team12/Data/ISkillService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-namespace Team12.Data {
-    public interface ISkillService {
-        Skill GetSkill(int skillId);
-        List<Skill> GetAllSkills();
-        bool UpdateSkill(Skill skill);
-        bool DeleteSkill(int skillId);
-    }
-}
